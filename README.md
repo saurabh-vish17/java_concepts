a34a93322238c38d96b7dc00d54cb23fbfd7a6ce
Sauarbh Vishwakarma <vish.saurabh07@gmail.com>
2025-12-06 20:33:37 +0530

    we write simple output program in java

--- README.md
+++ README.md
@@ -3,5 +3,4 @@ here you will get complete java language tutorial from basic
 
 topics in this repo.
 
-mainclass1: programs of simple input and output
-
+mainclass1: syntax programs of simple output
---
+++ mainclass1.java
@@ -0,0 +1,10 @@
+package java_concepts;
+//syntax of java main class
+
+public class MainClass1 {
+    // main method - starting point of java program
+    public static void main(String[] args) {
+        // print statement
+        System.out.println("Hello, World!");
+    }
+}
