f57581d13fb53b463b4cd46190adba42a11a898f
Sauarbh Vishwakarma <vish.saurabh07@gmail.com>
2025-12-07 22:26:31 +0530

    added the concept of string and its functions

--- Main2.java
+++ Main2.java
@@ -5,6 +5,8 @@ public class Main2 {
 // variable declaration and initialization
         int age=30;
         String school="mvm";
+        System.out.println("Age: "+age);
+        System.out.println("School: "+school);
 //data types in java(primitive and non-primitive)
 //primitive data types are: byte, short, int, long, float, double, char, boolean
         byte b=10;
@@ -16,9 +18,21 @@ public class Main2 {
         double num2=19.99;
         char grade='A'; 
         boolean isJavaFun=true;
+        System.out.println("Byte value: "+b);
+        System.out.println("Short value: "+s);
+        System.out.println("Integer value: "+num);
+        System.out.println("String value: "+name);
+        System.out.println("Long value: "+phoneNumber);
+        System.out.println("Float value: "+height);
+        System.out.println("Double value: "+num2);
+        System.out.println("Character value: "+grade);
+        System.out.println("Boolean value: "+isJavaFun);
+        
 //non-primitive data types are: String, Arrays, Classes, Interfaces
         String greeting="Hello, World!";
         int[] numbers={1,2,3,4,5};
+        System.out.println("Greeting: "+greeting);
+        System.out.print("Array elements: "+numbers[0]+", "+numbers[1]+", "+numbers[2]+", "+numbers[3]+", "+numbers[4]);
             
     }
 }
