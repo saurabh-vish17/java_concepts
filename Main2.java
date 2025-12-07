2204c56e9faa86f1d0fe1569b0f7c152b607a008
Sauarbh Vishwakarma <vish.saurabh07@gmail.com>
2025-12-07 22:14:01 +0530

    added the concepts of variable and data types

---
+++ Main2.java
@@ -0,0 +1,24 @@
+package java_concepts;
+
+public class Main2 {
+    public static void main(String[] args){
+// variable declaration and initialization
+        int age=30;
+        String school="mvm";
+//data types in java(primitive and non-primitive)
+//primitive data types are: byte, short, int, long, float, double, char, boolean
+        byte b=10;
+        short s=100;
+        int num=30;
+        String name="John";
+        long phoneNumber=1234567890L;
+        float height=5.9F;
+        double num2=19.99;
+        char grade='A'; 
+        boolean isJavaFun=true;
+//non-primitive data types are: String, Arrays, Classes, Interfaces
+        String greeting="Hello, World!";
+        int[] numbers={1,2,3,4,5};
+            
+    }
+}
