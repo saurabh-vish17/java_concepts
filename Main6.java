ac7eccd65a98e85331f117cfd69c53e4e27574d8
Sauarbh Vishwakarma <vish.saurabh07@gmail.com>
2025-12-10 22:47:53 +0530

    added the concepts of operator in java

---
+++ Main6.java
@@ -0,0 +1,38 @@
+package java_concepts;
+
+public class Main6 {
+    public static void main(String[] args){
+        //operators
+        
+        //1.arthimetic operators
+
+        int a=21;
+        int b=10;
+        int Sum=a+b;
+        int diff=a-b;
+        System.out.println(Sum);
+        System.out.println(diff);
+        int div=a/b;
+        int mod=a%b;
+        System.out.println(div);
+        System.out.println(mod);
+
+        //2.assignment operators(=,+=,-=,/=,%=,*=)
+
+        //unary operator
+        int num=2;
+        System.out.println(num);
+        num++;
+        System.out.println(num);
+        num--;
+        System.out.println(num);
+
+        //3. comparison operator
+        // ==
+        // !=
+        //>
+        //<
+        //>=
+        //<=
+    }
+}
