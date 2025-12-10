0788c4ee76d85b1128349080ae93995724ce7ecc
Sauarbh Vishwakarma <vish.saurabh07@gmail.com>
2025-12-10 22:58:43 +0530

    added the concepts of taking input from the user

---
+++ Main7.java
@@ -0,0 +1,13 @@
+package java_concepts;
+import java.util.Scanner;
+
+public class Main7 {
+    public static void main(String[] args){
+        //how to take input 
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter your age");
+        int age=sc.nextInt();
+        System.out.println(age);
+        
+    }
+}
