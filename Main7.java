87ca08a97243e1e9c9235e98f71cdb902f228a9a
Sauarbh Vishwakarma <vish.saurabh07@gmail.com>
2025-12-12 10:26:20 +0530

    added concepts how to take string as input

--- Main7.java
+++ Main7.java
@@ -3,11 +3,19 @@ import java.util.Scanner;
 
 public class Main7 {
     public static void main(String[] args){
-        //how to take input 
+        
         Scanner sc=new Scanner(System.in);
+
+        //how to take input
+
         System.out.println("enter your age");
         int age=sc.nextInt();
         System.out.println(age);
+
+        //to input string
+        String name=sc.nextLine();
+        System.out.println(name);
+
         
     }
 }
